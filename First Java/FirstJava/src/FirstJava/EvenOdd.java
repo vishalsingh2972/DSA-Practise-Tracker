db5007f3f8607aa4f205c93baf1f3632225bfc7f
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-08-07 20:36:52 +0530

    Update EvenOdd.java

--- First Java/FirstJava/src/FirstJava/EvenOdd.java
+++ First Java/FirstJava/src/FirstJava/EvenOdd.java
@@ -1,7 +1,7 @@
 package FirstJava;
 
 import java.util.Scanner;
-
+//Write a program to print whether a number is even or odd, also take input from the user
 public class EvenOdd {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
