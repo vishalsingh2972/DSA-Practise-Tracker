eeae4ac22d0e19654f299110b9b66d82fe4f993c
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-08-07 20:03:04 +0530

    Add files via upload

---
+++ First Java/FirstJava/src/FirstJava/Palindrome.java
@@ -0,0 +1,27 @@
+
+    package FirstJava;
+
+import java.util.Scanner;
+
+    public class Palindrome {
+        public static void main(String[] args) {
+            System.out.println("Check whether word is Palindrome or not");
+            System.out.print("Enter the string which you want to check:");
+            String original, reverse = "";
+            Scanner word = new Scanner(System.in);
+            original = word.nextLine();
+            int length = original.length();
+
+            for (int i = length - 1; i >= 0; i--)
+                reverse = reverse + original.charAt(i);
+            if (original.equals(reverse))
+                System.out.println(original + " is a Palindrome");
+            else
+                System.out.println(original + " is not a Palindrome");
+        }
+    }
+
+
+
+
+
