9591b3b52c88fca3a780e47fb220f643a9ae3ec4
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-08-13 13:40:06 +0530

    Add files via upload

---
+++ Conditional Loops/Basic/LoopsandConditionals/src/ReverseNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseNumber  {
+    public static void main(String[] args) {
+        Scanner Reverse = new Scanner(System.in);
+        int sidha = Reverse.nextInt();
+
+        int ans = 0;
+         while (sidha>0) {
+             int rem = sidha % 10;
+             sidha /=10; // sidha = sidha/10
+
+            ans = ans * 10 + rem;
+
+
+    }
+        System.out.println("The Reverse of the the number is "+ans);
+    }
+
+}
