bb8abeb80bb2fc265dc983ac108e48e59d6d031a
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-08-22 20:49:24 +0530

    Delete Arrays.java

--- Arrays & ArrayList/AA Basic/Arrays.java
+++
@@ -1,8 +0,0 @@
-public class Arrays {
-    public static void main(String[] args) {
-        int[] ros; // declaration of array. ros is getting defined in the stack
-        ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
-
- //     System.out.println(ros[5]); (0,1,2,3,4 indexes only)
-    }
-}
