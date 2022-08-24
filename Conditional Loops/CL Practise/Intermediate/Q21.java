e119230864a98ddc95f30bbf176403c1f2b8617a
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-08-24 22:00:09 +0530

    Update Q21.java

--- Conditional Loops/CL Practise/Intermediate/Q21.java
+++ Conditional Loops/CL Practise/Intermediate/Q21.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//Java program Vowel or consonant
+//Java program Vowel or Consonant
 public class Q21 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
