bf50c5fa9e696af3a8ad6b2836ae4186e5f9f1b1
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-08-27 18:12:58 +0530

    Add files via upload

---
+++ Conditional Loops/CL Practise/Intermediate/Q19.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+//HCF of Two Numbers Program
+public class Q19 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the numbers : ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int max = 0;
+        System.out.print("The Common Factors of the both numbers are : ");
+
+
+        for (int i = 1; i <= a; i++)
+        {
+            if (a % i == 0 && b % i == 0)
+            {
+                System.out.print(i + ","); //prints all common factors of a and b;
+                max = i;
+            }
+
+        }
+        System.out.println();
+        System.out.println("Highest Common Factor is " + max);
+    }
+}
