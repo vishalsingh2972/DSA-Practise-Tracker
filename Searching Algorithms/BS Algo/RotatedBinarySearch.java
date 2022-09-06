a71636972192f2d990055d980ae17612b3cdb3b1
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-09-07 04:45:56 +0530

    Update RotatedBinarySearch.java

--- Searching Algorithms/BS Algo/RotatedBinarySearch.java
+++ Searching Algorithms/BS Algo/RotatedBinarySearch.java
@@ -2,7 +2,9 @@
 //https://leetcode.com/problems/search-in-rotated-sorted-array/
 public class RotatedBinarySearch {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int[] arr = {4, 5, 6, 7, -1, 0, 1, 2};
+        int target = 0;
+
 
     }
 
