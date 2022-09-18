967cf4adf7d892f7b994e258ca371091795a3506
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-09-18 20:43:33 +0530

    Update Floor.java

--- Searching Algorithms/BS Algo/Floor.java
+++ Searching Algorithms/BS Algo/Floor.java
@@ -3,11 +3,12 @@ public class Floor {
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 9, 14, 16, 18};
         int target =  1; //gives output -1 as end becomes -1
+        //int target = 10;
         int ans = floor(arr, target);
         System.out.println(ans);
     }
 
-    // return the index of greatest number <= target
+    // return the index of the greatest number <= target
     static int floor(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
@@ -29,3 +30,4 @@ public class Floor {
         return end;
     }
 }
+
