2692ede26fdc26f556ba7c0c1994570c090c33e9
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-09-18 21:53:51 +0530

    Update RotatedBinarySearch.java

--- Searching Algorithms/BS Algo/RotatedBinarySearch.java
+++ Searching Algorithms/BS Algo/RotatedBinarySearch.java
@@ -3,11 +3,11 @@
 public class RotatedBinarySearch {
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, -1, 0, 1, 2};
-        int target = 0;
+        int target = 1;
+        System.out.println(search(arr, target));
 
     }
 
-    
         public static int search(int[] nums, int target) {
             int pivot = findPivot(nums);
             // if you did not find a pivot, it means the array is not rotated
@@ -17,9 +17,9 @@ public class RotatedBinarySearch {
             }
 
             //if pivot is found, we have 2 sorted arrays on each side of pivot
-            // if (nums[pivot] == target) {
-            //     return pivot;
-            // }
+             if (nums[pivot] == target) {
+                 return pivot;
+             }
 
             if (target >= nums[0]) {
                 return binarySearch(nums, target, 0, pivot ); //pivot also included so target = pivot condition not required
@@ -29,35 +29,35 @@ public class RotatedBinarySearch {
             return binarySearch(nums, target, pivot + 1, nums.length - 1);
         }
 
-    
         public static int findPivot(int[] arr) {
             int start = 0;
             int end = arr.length - 1;
             while (start <= end) {
                 int mid = start + (end - start) / 2;
-                // 4 cases over here
-                if ( mid < end && arr[mid] > arr[mid + 1]) {
+                // 5 cases over here (KK used 4 cases, done that on Leetcode)
+                if ( mid>start && mid<end && arr[mid] > arr[mid+1] && arr[mid] > arr[mid - 1]) {
+                    return mid-1;
+                }
+                if (mid<end && arr[mid] > arr[start] && arr[mid] > arr[mid + 1] ) {
                     return mid;
                 }
-                if (mid > start && arr[mid] < arr[mid - 1]) {
-                    return mid-1;
+                if ( mid>start && arr[mid-1] > arr[mid]) {
+                    return mid - 1;
                 }
                 if (arr[mid] <= arr[start]) {
                     end = mid - 1;
                 }
                 if (arr[mid] > arr[start]){
-                    start = mid + 1;
+                    start = mid;
                 }
             }
 
             return -1;
-
         }
-
-
         public static int binarySearch(int[] ARR, int TARGET, int start, int end){
+
             while( start <= end){
-                
+
                 int MID = start + (end-start)/2;
                 if (TARGET < ARR[MID]) {
 
@@ -78,5 +78,4 @@ public class RotatedBinarySearch {
         }
 
     }
-
 
