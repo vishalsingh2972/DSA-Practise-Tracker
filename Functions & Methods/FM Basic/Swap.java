767fbad8a29ab8b45c8c9c0b9732dacbbd4cf32d
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-09-27 18:05:11 +0530

    Update Swap.java

--- Functions & Methods/FM Basic/Swap.java
+++ Functions & Methods/FM Basic/Swap.java
@@ -1,27 +1,24 @@
-
-//Swap using Functions //Function Scope Concept
 public class Swap {
     public static void main(String[] args) {
         int a = 40;
         int b = 50;
-
         swap(a, b);
         System.out.println("a value is "+a);
         System.out.println("b value is "+b);
-
     }
     static void swap(int a, int b) {
         int temp = a;
-        a = b;    //we are using primitive datatypes so only value is passed here,hence making any changes here will not cause any change in the original values (present in main function above)
+        a = b;
+        //we are using primitive datatypes so only value is passed here,
+        // hence making any changes here will not cause any change in the original values (present in main function above)
         b = temp;
         // this change will only be valid in this function scope only.
         System.out.println("a value is (inside swap) "+a);
         System.out.println("b value is (inside swap) "+b);
-
-     }
+    }
 }
 
-//Normal code for Swap 
+//Normal code for Swap
 /*
 public class Swap {
     public static void main(String[] args) {
@@ -45,5 +42,5 @@ public class Swap {
 //
 //static void changeName(String naam) {
 //        naam = "Rahul Rana"; // creating a new object
-// here change made in naam did not reflect in name as here we are not making any chnages changing the existing object "Kunal Kushwaha" we are creating a new object "Rahul Rana"
+// here change made in naam did not reflect in name as here we are not changing the existing object "Kunal Kushwaha" we are creating a new object "Rahul Rana"
 //        }
