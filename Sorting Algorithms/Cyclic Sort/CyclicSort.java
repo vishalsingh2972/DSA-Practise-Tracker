b1f8ff81539960f1df6367f95d4c2bca8d635c5a
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-10-27 20:58:57 +0530

    Add files via upload

---
+++ Sorting Algorithms/Cyclic Sort/CyclicSort.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+      public static void main(String[] args) {
+        int[] ARR = {3, 5, 2, 1, 4};
+        //System.out.println(Arrays.toString(ARR));
+        sort(ARR);
+        System.out.println(Arrays.toString(ARR));
+        }
+      static void sort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+           int correctIndex = arr[i] - 1;
+        //if element not present at correct index swap
+            if(arr[i] != arr[correctIndex]){
+        swap(arr, i, correctIndex);
+              }
+            else{
+        i++;
+              }
+           }
+        }
+
+static void swap(int[] arr, int i, int correctIndex){
+        int temp =  arr[i];
+        arr[i] =  arr[correctIndex];
+        arr[correctIndex] = temp;
+        }
+ }
+
+
+// ALTERNATE WAY (getting modified array as return from sort function)
+
+//public class CyclicSort {
+//    public static void main(String[] args) {
+//        int[] ARR = {3, 5, 2, 1, 4};
+//        int[] ans = sort(ARR);
+//        System.out.println(Arrays.toString(ans));
+//    }
+//
+//    static int[] sort(int[] arr){
+//        int i = 0;
+//        while(i < arr.length){
+//            int correctIndex = arr[i] - 1;
+//            //if element not present at correct index swap
+//            if(arr[i] != arr[correctIndex]){
+//                swap(arr, i, correctIndex);
+//            }
+//            else{
+//                i++;
+//            }
+//
+//        }
+//      return arr;
+//    }
+//
+//    static void swap(int[] arr, int i, int correctIndex){
+//        int temp =  arr[i];
+//        arr[i] =  arr[correctIndex];
+//        arr[correctIndex] = temp;
+//    }
+//
+//}
+
+
+
