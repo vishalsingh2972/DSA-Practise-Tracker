bb6d5f8776cfd2e6023f37a1a8e3138649c6bb48
Vishal Singh <106817047+vishalsingh2972@users.noreply.github.com>
2022-11-04 16:28:46 +0530

    Update Son.java

--- OOPS/OOP 5/AbstractDemo/Son.java
+++ OOPS/OOP 5/AbstractDemo/Son.java
@@ -3,7 +3,7 @@ package AbstractDemo;
 public class Son extends Parent{
 //    public class Son extends Parent, Parent2 { //Multiple Inheritances is not supported in Abstract Classes (Not Possible)
     public Son(int age) { //child class constructor
-        super(100); //we cannot create objects of PC, so we cannot call constructor of PC directly, but we can call PC constructor indirectly via the CC constructor using super keyword in the CC constructor (like used here)
+        super(100); //we cannot create objects of PC(as it is abstract class), so we cannot call constructor of PC directly, but we can call PC constructor indirectly via the CC constructor using super keyword in the CC constructor (like used here)
 //        System.out.println(age);
         this.age = age;
     }
