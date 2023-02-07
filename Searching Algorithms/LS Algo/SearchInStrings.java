9151731d4a7737c8ab550627bc3ff75235f98ad5
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-02-07 10:05:20 +0530

    Update SearchInStrings.java

--- Searching Algorithms/LS Algo/SearchInStrings.java
+++ Searching Algorithms/LS Algo/SearchInStrings.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class SearchInStrings {
     public static void main(String[] args) {
         String name = "Vishal";
-        char Target = 'i';
+        char Target = 's';
         //System.out.println(search(name, Target));
-        //System.out.println(search2(name, Target));
+        System.out.println(search2(name, Target));
         System.out.println(Arrays.toString(name.toCharArray())); // This statement is going to create a character array (i.e here it is converting string input into a character array)
 
                                                  //java string toCharArray() method converts this string into character array
