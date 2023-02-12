7ef7b9b5927b8129232f2fdd0c11ae2c44fc3ca3
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-02-12 23:11:03 +0530

    Update M12.java

--- Searching Algorithms/Searching Practise/M12.java
+++ Searching Algorithms/Searching Practise/M12.java
@@ -4,13 +4,10 @@ public class M12 {
 
         int[][] arr = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         int target = 3;
+//        int target = 2;
 //        System.out.println(searchMatrix(arr, target));
 
-        int[][] arr2 = {{1,3}};
-        int target2 = 3;
-//        System.out.println(searchMatrix2(arr2, target2));
-
-        System.out.println(searchMatrix2(arr,target));
+        System.out.println(searchMatrix2(arr, target));
 
     }
 
