175f49052015453c9fd4d6bdbb02440bc45e9b86
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-03-20 01:32:14 +0530

    Update E6.java

--- Searching Algorithms/Searching Practise/E6.java
+++ Searching Algorithms/Searching Practise/E6.java
@@ -2,8 +2,10 @@
 public class E6 {
     public static void main(String[] args) {
 
-        System.out.println(arrangeCoins(5));
-        System.out.println(arrangeCoins(8));
+//        System.out.println(arrangeCoins(5));
+//        System.out.println(arrangeCoins(8));
+
+        System.out.println(arrangeCoins(1804289383));
 
     }
 
@@ -16,8 +18,11 @@ public class E6 {
         while(start <= end){
 
             long mid = start + (end-start)/2;
-            long sum = mid*(mid+1)/2;
+//            System.out.println(mid);
+            long sum = mid*(mid+1)/2; //if limit of n = limit of int as given in the question = 2^31 - 1, then value of sum(approx calculation not exact) = n/2 * (n/2 + 1) /2 = n + (n^2 - 6n)/4 , now this value n + (n^2 - 6n)/4 in most of the cases will exceed n i.e the limit of int , so it is better to take all long to accommodate all the values without any integer overflow (An integer overflow can cause the value to wrap and become negative, which violates the program's assumption and may lead to unexpected behavior)
+                                      //like for n = 1804289383, mid = 902144692, and sum = 406932523102959778 >>>>>>>>>>>>>>>>> limit of int (= 2^31 - 1 = 2147483647), hence we need to use long in order to store such higher values like sum
 
+//            System.out.println(sum);
             if(sum > n) {
                 end = mid - 1;
             }
@@ -25,7 +30,6 @@ public class E6 {
             else if (sum <= n) {
                 start = mid + 1;
                 ans = mid;
-
             }
 
         }
