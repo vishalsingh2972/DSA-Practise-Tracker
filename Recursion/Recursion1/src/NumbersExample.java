525e65f99558ba1aaaf4472f4ed9667b788dfd03
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-07-27 21:44:30 +0530

    try

--- Recursion/Recursion1/src/NumbersExample.java
+++ Recursion/Recursion1/src/NumbersExample.java
@@ -32,12 +32,4 @@ public class NumbersExample {
 
 }
 
-// - function calling another function
-// - all functions have 1 thing in common - the body and the definition of the functions is same
-
-//VVI
-// all the function calls that happen in a programming language they go into the stack memory |_|
-// - while the function is not finished executing it will remain in the stack memory
-// in java the first function that is called is the main function so the main function is the one that will go first into the stack and the last function hat will come out of the stack, and when a function is staying inside the stack it basically means that function call is currently going on
-// - when a function finishes executing, it is removed from the stack and the flow of the program is restored to the place where that function was called
 
