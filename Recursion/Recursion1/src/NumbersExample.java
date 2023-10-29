9be6f4ae886f880a24b987b68189e497b2e0ef2f
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-10-29 21:59:10 +0530

    done

--- Recursion/Recursion1/src/NumbersExample.java
+++ Recursion/Recursion1/src/NumbersExample.java
@@ -37,8 +37,10 @@ public class NumbersExample {
 // - all functions have 1 thing in common - the body and the definition of the functions is same
 
 //VVI
-// all the function calls that happen in a programming language they go into the stack memory |_|
-// - while the function is not finished executing it will remain in the stack memory
-// in java the first function that is called is the main function so the main function is the one that will go first into the stack and the last function that will come out of the stack, and when a function is staying inside the stack it basically means that function call is currently going on
+// all the function calls that happen in a programming language they go into the stack memory |_| , here the fact that the function calls go into the stack memory means that the execution of a program is a last-in-first-out (LIFO) process. This means that the last function to be called is the first function to return.
+// - while the function is not finished executing it will remain in/inside the stack memory.
+// in java the first function that is called is the main function so the main function is the one that will go first into the stack memory and the last function that will come out of the stack memory, and when a function is staying inside the stack it basically means that the function call is currently going on for that function.
 // - when a function finishes executing, it is removed from the stack and the flow of the program is restored to the place where that function was called
+
+// primitives are also stored in the stack memory along with the functions
 
