98c766ea7662482dde032c986908d37e0af60fc0
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-11-06 20:49:26 +0530

    done

--- Recursion/Recursion1/src/NumbersExampleRecursion.java
+++ Recursion/Recursion1/src/NumbersExampleRecursion.java
@@ -25,3 +25,17 @@ public class NumbersExampleRecursion {
 // {3} Each function call is a separate function call and will occupy a separate memory in the stack, so basically if you are calling a function again and again you can treat it as a separate call in the stack memory like we did in this example where even though print(5), print(4), print(3), print(2) are all being called recursively from inside of print(1) one after the other but still all these 4 - print(5), print(4), print(3), print(2) will still occupy separate memory in the stack apart from the memory occupied by print(1) function // so here evn though the same function print(int n) is being called again and again but still every function call will occupy a separate memory in the stack memory i.e as many times as you are calling that function each time it will take memory separately
 // No Base Condition ---> Function calls will keep happening, stack will keep getting filled again and again and we know that every function call will take up some memory in the stack, so if there is no base condition this will keep going keep going on and on....and at one point of time it will reach a stage where memory of computer will exceed the limit and this will give StackOverflowError
 
+// Why Recursion? - It helps us in solving bigger/complex problems in a simpler way.
+// You can convert recursion solution into an iteration (iteration means not using any function calls, and just making use of loops) and vice versa. //So the idea is first solve the complex problem using Recursion then convert it into an iteration in order to get a more optimized answer, because directly solving the complex problem using iteration is difficult and that is why we need Recursion.
+
+///
+//When using Recursion to print numbers from 1 to 10 -  space complexity = space complexity of N = O(N) as each function call will occupy separate space in the stack memory // Hence space complexity is not constant when using Recursion because of the function calls/Recursive calls.
+//When we use iteration or when we run a for loop to print numbers from 1 to 10 -  space complexity = constant space complexity = O(1)  as irrespective of which number is being printed the whole for loop will occupy whole for loop will occupy only 1 part of the stack memory
+
+// Recursion helps us in breaking down bigger problems into smaller problems
+
+// Visualizing Recursion: by using Recursion Tree (also known as a recursive tree)
+// How to check whether a problem can be solved using Recursion - see if the problem can be broken down into smaller problems, and when we write Recursion in the form of a formula like Big Problem =  SmallProblem1 + SmallProblem2 it is called as Recurrence Relation, so any Recursive solution you can convert into the form of a formula and that is known as Recurrence Relation, for example in case of Fibonacci numbers F(N) = F(N-1) + F(N-2) is the Recurrence Relation.
+// also in the above example Fibonacci numbers F(N) = F(N-1) + F(N-2), in this RHS can further be broken down as F(N-1) = F(N-2) + F(N-3) & F(N-2) = F(N-3) + F(N-4) and so on......this will continue till we reach the known values i.e F(0) = 0 and F(1) = 1,this is known as the Base Condition or Base Condition is represented by the answers we already know so in this case base condition is values F(0) = 0 and F(1) = 1, so now we understood how to figure out base condition when we are using Recursion.
+
+
