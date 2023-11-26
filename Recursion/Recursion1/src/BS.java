116ed0aae22ec76cc5ac23ec4c7220a7b569d170
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-11-26 19:53:06 +0530

    Update BS.java

--- Recursion/Recursion1/src/BS.java
+++ Recursion/Recursion1/src/BS.java
@@ -33,3 +33,6 @@ public class BS {
 //✨🌟🧨 VVVVIP 🧨✨🌟 (variables in recursion 2 [continuation])
 // if there are variables that you need to pass in the future function calls, put it inside the arguments or in the arguments of the function - like here 's' and 'e'
 // if the variable is valuable in that particular function call only and that we don't need to pass in the future recursive calls, put those values in the body of the function - like here 'm'
+
+//✨🌟🍕 VVVVIP 🍕✨🌟
+// make sure to return the result of a function call because if you don't return it in the end it will be called after all the sub problems are solved and in the end it will be calling the main function, if the original one is not returned the main one will also be not returned so your answer will not be returned so return the whatever answer you're getting
