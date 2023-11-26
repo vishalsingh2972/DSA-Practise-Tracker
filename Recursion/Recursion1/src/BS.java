e51a2c74dc294626780ae691cca784653c3407a9
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-11-26 19:39:57 +0530

    done

---
+++ Recursion/Recursion1/src/BS.java
@@ -0,0 +1,35 @@
+//Binary Search using Recursion
+
+public class BS {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 55, 66, 78};
+        int target = 66;
+        System.out.println("The target is present at " + search(array, target, 0, array.length - 1) + " index position");
+    }
+
+    static int search(int[] arr, int target, int s, int e){
+
+        if(s > e){ //not found the answer condition
+            return -1;
+        }
+
+        int m = s + (e - s)/2;
+
+        if(arr[m] == target){
+            return m;
+        }
+
+        if(target < arr[m]){
+        //so now e = m - 1;
+            return search(arr, target, s, m - 1);
+        }
+
+        //else now s = m + 1;
+        return search(arr, target, m + 1, e);
+    }
+
+}
+
+//✨🌟🧨 VVVVIP 🧨✨🌟 (variables in recursion 2 [continuation])
+// if there are variables that you need to pass in the future function calls, put it inside the arguments or in the arguments of the function - like here 's' and 'e'
+// if the variable is valuable in that particular function call only and that we don't need to pass in the future recursive calls, put those values in the body of the function - like here 'm'
